3c55403a0e7a9f42c6e47be7301fd0965901b0a0
LindonBerisha <lindon-berisha@hotmail.com>
2024-02-15 18:39:33 +0100

    Erste JS Übungen beendet!

--- 4AAIF/4AAIF_Berisha_ErstesJS/script.js
+++ 4AAIF/4AAIF_Berisha_ErstesJS/script.js
@@ -1,1 +1,20 @@
-alert("Hallo")
+let text1 = "Hallo 4AAIF";
+let anzahl = 18;
+alert(text1)
+console.log(text1+", Heute sind " + anzahl + " Schüler anwesend!")
+const wert = 10.9;
+//wert = 20; <--- unmöglich bei const
+console.log(typeof wert);
+let wert2 = 18
+console.log(typeof wert2);
+console.log(typeof text1);
+
+let o = {vorname: "Hans", nachname: "Mustermann"};
+console.log(typeof o);
+console.log("Objekt: "+ o);
+
+let x1 = "18";
+let x2 = 18;
+
+console.log('18 == "18" ?', (x1 == x2) ); // Inhaltsgleichheit wird geprüft
+console.log('18 === "18" ?', (x1 === x2) ); // Inhalts- und Typgleichheit wird geprüft
